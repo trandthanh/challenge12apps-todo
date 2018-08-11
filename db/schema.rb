e5134a33ea7ea41d68f1c2b4a8193cedd2a09dd9
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-08-11 15:26:58 +0200

    Added the Mark as Complete functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_112602) do
+ActiveRecord::Schema.define(version: 2018_08_11_131912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_08_11_112602) do
     t.bigint "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
